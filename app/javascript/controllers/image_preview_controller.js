025ff7130f612c4227dd8cb1a112b2755bc43479
Maxime Oudin <oudinmaxime@gmail.com>
2024-03-23 20:41:10 +0100

    prviex pour change et set manage

--- app/javascript/controllers/image_preview_controller.js
+++ app/javascript/controllers/image_preview_controller.js
@@ -5,20 +5,28 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["input", "preview", "actual"];
 
+  connect() {
+    this.previewImage();
+  }
+
   previewImage() {
-    console.log('methode previewImage appelée')
+    console.log('Méthode previewImage appelée');
     const input = this.inputTarget;
     const preview = this.previewTarget;
     const actual = this.actualTarget;
+
     if (input.files && input.files[0]) {
       const reader = new FileReader();
 
       reader.onload = function(e) {
         preview.innerHTML = `<p>Prévisualisation du document : </p><img src="${e.target.result}" style="max-width: 200px;">`;
-        actual.innerHTML = "";
+
+        if (actual) { // Vérifie si l'élément actual existe
+          actual.innerHTML = ""; // Efface le contenu de l'élément actual
+        }
       };
 
-      reader.readAsDataURL(input.files[0]);
+      reader.readAsDataURL(input.files[0]); // Lit le contenu du fichier
     }
   }
 }
